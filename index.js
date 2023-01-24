1c29dedd9ddeee71901dc2c6461d0e9dc5479107
Nolu-M <119700411+Nolu-M@users.noreply.github.com>
2023-01-24 21:18:34 +0200

    Changes

---
+++ index.js
@@ -0,0 +1,11 @@
+//document.getElementById("count").innerText = 5
+
+//change  the count-el in the html to reflect the new count
+
+let count = 0
+
+function increment() {
+    console.log("clicked")
+    count = count + 1
+    console.log(count)
+}
